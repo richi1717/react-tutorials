053c55e0c0a501e551afbc26b0d902673e8c5769
Richi Jeffery <kikki_sawn@yahoo.com>
2016-01-01 21:55:32 -0700

    add tutorial projects

---
+++ products/server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var PRODUCTS_FILE = path.join(__dirname, 'products.json');
+
+//  Set port
+app.set('port', (process.env.PORT || 3000));
+
+//  Set Static path
+app.use(express.static(path.join(__dirname, 'client')));
+
+//  BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Get Products
+app.get('/api/products', function (req, res) {
+  fs.readFile(PRODUCTS_FILE, function (error, data) {
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  });
+})
+
+// Add Products
+app.post('/api/products', function (req, res) {
+  fs.readFile(PRODUCTS_FILE, function (err, data) {
+    var products = JSON.parse(data);
+    products.push(req.body);
+    fs.writeFile(PRODUCTS_FILE, JSON.stringify(products, null, 3), function (err) {
+      res.setHeader('Cache-Control', 'no-cache');
+      res.json(products);
+    });
+  });
+});
+
+
+//  Start Server
+app.listen(app.get('port'), function () {
+  console.log('Server has started on port: ' + app.get('port'));
+});
